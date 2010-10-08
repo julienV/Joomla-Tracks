217e00d3565765de84be6c344d5a15989e5cd020
Julien Vonthron <jlv@jlv-solutions.com>
2010-10-08 18:46:38 +0200

    / remove tracks tables on uninstall

---
+++ com_tracks/administrator/components/com_tracks/uninstall.mysql.nonutf8.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `#__tracks_projects`, 
+                     `#__tracks_competitions`, 
+                     `#__tracks_seasons`, 
+                     `#__tracks_teams`, 
+                     `#__tracks_clubs`, 
+                     `#__tracks_individuals`, 
+                     `#__tracks_rounds`, 
+                     `#__tracks_subroundtypes`, 
+                     `#__tracks_projects_rounds`, 
+                     `#__tracks_projects_subrounds`, 
+                     `#__tracks_projects_individuals`, 
+                     `#__tracks_projects_teams`, 
+                     `#__tracks_rounds_results`, 
+                     `#__tracks_project_settings`
+                     ;
+
