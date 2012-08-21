b7c962644ca95bc0f20736565924b759754d3f02
julien <jlv@jlv-solutions.com>
2012-08-21 15:32:53 +0200

    / js fix

--- com_tracks/administrator/components/com_tracks/views/subroundresults/tmpl/default.php
+++ com_tracks/administrator/components/com_tracks/views/subroundresults/tmpl/default.php
@@ -49,6 +49,30 @@ JHTML::_('behavior.tooltip');
 </style>
 
 <script language="javascript" type="text/javascript">
+
+window.addEvent('domready', function () {
+	var searchInput = $('quickadd2');
+
+	//A simple spinner div, display-toggled during request
+	var indicator = new Element('div', {
+	 'class': 'autocompleter-loading',
+	 'styles': {'display': 'none'}
+	}).injectAfter($('submit2')); // appended after the input
+	
+	var completer = new Autocompleter.Ajax.Jsonindividual(searchInput, 
+		'<?php echo $this->site_url;?>administrator/index.php?option=com_tracks&controller=quickadd&task=search', {
+	 'postVar': 'query',
+	 'minLength': 3,
+	 'onRequest': function(el) {
+	     indicator.setStyle('display', '');
+	 },
+	 'onComplete': function(el) {
+	     indicator.setStyle('display', 'none');
+	 }
+	});
+
+});
+
 function submitbutton(pressbutton)
 {
   if (pressbutton == "saveranks"){
