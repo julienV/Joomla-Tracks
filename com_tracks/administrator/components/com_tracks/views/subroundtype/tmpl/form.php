cec030e14eeac7ac5064bafde11bd57f23fbb8cb
julien <jlv@jlv-solutions.com>
2013-04-08 15:53:40 +0200

    / fixed subround type edit layout

--- com_tracks/administrator/components/com_tracks/views/subroundtype/tmpl/form.php
+++ com_tracks/administrator/components/com_tracks/views/subroundtype/tmpl/form.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version    $Id: form.php 95 2008-05-02 10:33:44Z julienv $ 
+* @version    $Id: form.php 95 2008-05-02 10:33:44Z julienv $
 * @package    JoomlaTracks
 * @copyright	Copyright (C) 2008 Julien Vonthron. All rights reserved.
 * @license		GNU/GPL, see LICENSE.php
@@ -32,7 +32,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
     JToolBarHelper::help( 'screen.tracks.edit', true );
 ?>
 
-<script language="javascript" type="text/javascript">
+<script type="text/javascript">
 	Joomla.submitbutton = function (pressbutton) {
 		var form = document.adminForm;
 		if (pressbutton == 'cancel') {
@@ -51,47 +51,49 @@ defined('_JEXEC') or die('Restricted access'); ?>
 </script>
 
 <div id="tracksmain">
-<form action="index.php" method="post" name="adminForm" id="adminForm">
-<div class="width-70">
-<fieldset class="adminform"><legend><?php echo JText::_('COM_TRACKS_Subround_type' ); ?></legend>
+	<form action="index.php" method="post" name="adminForm" id="adminForm">
+		<div class="width-70">
+			<fieldset class="adminform">
+				<legend><?php echo JText::_('COM_TRACKS_Subround_type' ); ?></legend>
 
-	<ul class="adminformlist">
-		<li><label for="name"> <?php echo JText::_('COM_TRACKS_Name' ); ?>:
-			</label>
-			<input class="text_area required" type="text" name="name" id="name"
-			size="32" maxlength="40" value="<?php echo $this->object->name?>" />
-		</li>
-		<li>
-		<label for="alias"> <?php echo JText::_('COM_TRACKS_Alias' ); ?>:
-		</label>
-		<input class="text_area" type="text" name="alias" id="alias"
-			size="32" maxlength="250" value="<?php echo $this->object->alias?>" />
-		</li>
-		<li><label for="alias"> <?php echo JText::_('COM_TRACKS_Note' ); ?>:
-		</label>
-		<input class="text_area" type="text" name="note" id="note"
-			size="32" maxlength="100" value="<?php echo $this->object->note?>" />
-		</li>
-		<li>
-		<label for="points_attribution"> <?php echo JText::_('COM_TRACKS_Points' ); ?>:
-		</label>
-		<input class="text_area" type="text" name="points_attribution" id="points_attribution"
-			size="50" maxlength="255" value="<?php echo $this->object->points_attribution?>" />
-		</li>
-		<li>
-		<label for="description"> <?php echo JText::_('COM_TRACKS_Description' ); ?>:
-			</label>
-			<?php echo $this->editor->display('description', $this->object->description, '100%', '400', '70', '15'); ?>
-		</li>
-	</ul>
-</fieldset>
-</div>
+				<ul class="adminformlist">
+					<li><label for="name"> <?php echo JText::_('COM_TRACKS_Name' ); ?>:
+						</label>
+						<input class="text_area required" type="text" name="name" id="name"
+						size="32" maxlength="40" value="<?php echo $this->object->name?>" />
+					</li>
+					<li>
+						<label for="alias"> <?php echo JText::_('COM_TRACKS_Alias' ); ?>:
+						</label>
+						<input class="text_area" type="text" name="alias" id="alias"
+							size="32" maxlength="250" value="<?php echo $this->object->alias?>" />
+					</li>
+					<li><label for="alias"> <?php echo JText::_('COM_TRACKS_Note' ); ?>:
+						</label>
+						<input class="text_area" type="text" name="note" id="note"
+							size="32" maxlength="100" value="<?php echo $this->object->note?>" />
+					</li>
+					<li>
+						<label for="points_attribution"> <?php echo JText::_('COM_TRACKS_Points' ); ?>:
+						</label>
+						<input class="text_area" type="text" name="points_attribution" id="points_attribution"
+							size="50" maxlength="255" value="<?php echo $this->object->points_attribution?>" />
+					</li>
+				</ul>
 
-<div class="clr"></div>
+				<div class="clr"></div>
+				<label for="description"> <?php echo JText::_('COM_TRACKS_Description' ); ?>:</label>
+				<div class="clr"></div>
+				<?php echo $this->editor->display('description', $this->object->description, '100%', '400', '70', '15'); ?>
 
-<input type="hidden" name="option" value="com_tracks" />
-<input type="hidden" name="controller" value="subroundtype" />
-<input type="hidden" name="cid[]" value="<?php echo $this->object->id; ?>" />
-<input type="hidden" name="task" value="" />
-</form>
+			</fieldset>
+		</div>
+
+	<div class="clr"></div>
+
+	<input type="hidden" name="option" value="com_tracks" />
+	<input type="hidden" name="controller" value="subroundtype" />
+	<input type="hidden" name="cid[]" value="<?php echo $this->object->id; ?>" />
+	<input type="hidden" name="task" value="" />
+	</form>
 </div>
