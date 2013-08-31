b4b31c16427e67018a6faaad96116281ba9255ed
julien <jlv@jlv-solutions.com>
2013-08-31 17:12:13 +0200

    seasons

--- component/admin/tables/competition.php
+++ component/admin/tables/competition.php
@@ -2,8 +2,8 @@
 /**
 * @version    $Id: competition.php 10 2008-02-03 13:19:59Z julienv $
 * @package    JoomlaTracks
-* @copyright	Copyright (C) 2008 Julien Vonthron. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
+* @copyright  Copyright (C) 2008 Julien Vonthron. All rights reserved.
+* @license    GNU/GPL, see LICENSE.php
 * Joomla Tracks is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
 * is derivative of works licensed under the GNU General Public License or
@@ -11,7 +11,7 @@
 * See COPYRIGHT.php for copyright notices and details.
 */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
 // Include library dependencies
@@ -20,17 +20,17 @@ jimport('joomla.filter.input');
 /**
 * Competitions Table class
 *
-* @package		Tracks
-* @since 0.1
+* @package  Tracks
+* @since    0.1
 */
 class TracksTableCompetition extends FOFTable
 {
 	/**
 	 * constructor
 	 *
-	 * @param   string   $table  name of the table
-	 * @param   string   $key    table primary key
-	 * @param   database &$db    A database connector object
+	 * @param   string           $table  name of the table
+	 * @param   string           $key    table primary key
+	 * @param   JDatabaseDriver  &$db    A database connector object
 	 */
 	public function __construct($table, $key, &$db)
 	{
@@ -41,7 +41,9 @@ class TracksTableCompetition extends FOFTable
 	 * Overloaded check method to ensure data integrity
 	 *
 	 * @access public
+	 *
 	 * @return boolean True on success
+	 *
 	 * @since  1.0
 	 */
 	function check()
--- component/admin/tables/season.php
+++ component/admin/tables/season.php
@@ -1,56 +1,61 @@
 <?php
 /**
-* @version    $Id: season.php 10 2008-02-03 13:19:59Z julienv $ 
-* @package    JoomlaTracks
-* @copyright	Copyright (C) 2008 Julien Vonthron. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla Tracks is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version    $Id: competition.php 10 2008-02-03 13:19:59Z julienv $
+ * @package    JoomlaTracks
+ * @copyright  Copyright (C) 2008 Julien Vonthron. All rights reserved.
+ * @license    GNU/GPL, see LICENSE.php
+ * Joomla Tracks is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
 // Include library dependencies
 jimport('joomla.filter.input');
 
 /**
-* Season Table class
-*
-* @package		Tracks
-* @since 0.1
-*/
-class TableSeason extends JTable
+ * Competitions Table class
+ *
+ * @package  Tracks
+ * @since    0.1
+ */
+class TracksTableSeason extends FOFTable
 {
 	/**
-	 * Constructor
+	 * constructor
 	 *
-	 * @param object Database connector object
-	 * @since 1.0
+	 * @param   string           $table  name of the table
+	 * @param   string           $key    table primary key
+	 * @param   JDatabaseDriver  &$db    A database connector object
 	 */
-	function __construct(& $db) {
+	public function __construct($table, $key, &$db)
+	{
 		parent::__construct('#__tracks_seasons', 'id', $db);
 	}
 
 	/**
 	 * Overloaded check method to ensure data integrity
 	 *
-	 * @access public
-	 * @return boolean True on success
-	 * @since 1.0
+	 * @access  public
+	 *
+	 * @return  boolean True on success
+	 *
+	 * @since   1.0
 	 */
 	function check()
 	{
-    $alias = JFilterOutput::stringURLSafe($this->name);
+		$alias = JFilterOutput::stringURLSafe($this->name);
 
-    if(empty($this->alias) || $this->alias === $alias ) {
-      $this->alias = $alias;
-    }
-		//should check name unicity
+		if (empty($this->alias) || $this->alias === $alias)
+		{
+			$this->alias = $alias;
+		}
+
+		// Should check name unicity
 		return true;
 	}
 }
-?>
