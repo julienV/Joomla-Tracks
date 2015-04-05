74c0b5e3644852be568a26918edc37788ce84d99
julien <jlv@jlv-solutions.com>
2015-04-05 20:38:36 +0200

    Working on results

--- component/admin/models/eventresults.php
+++ component/admin/models/eventresults.php
@@ -1,26 +1,21 @@
 <?php
 /**
- * @version    0.2
- * @package    JoomlaTracks
- * @copyright  Copyright (C) 2008 Julien Vonthron. All rights reserved.
- * @license    GNU/GPL, see LICENSE.php
- *             Joomla Tracks is free software. This version may have been modified pursuant
- *             to the GNU General Public License, and as distributed it includes or
- *             is derivative of works licensed under the GNU General Public License or
- *             other free or open source software licenses.
- *             See COPYRIGHT.php for copyright notices and details.
+ * @package     Tracks
+ * @subpackage  Admin
+ * @copyright   Tracks (C) 2008-2015 Julien Vonthron. All rights reserved.
+ * @license     GNU General Public License version 2 or later
  */
 
-// Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 
 /**
- * Joomla Tracks Component sub-round results Model
+ * Tracks Component Event Results Model
  *
- * @package  Tracks
- * @since    0.2
+ * @package     Tracks
+ * @subpackage  Admin
+ * @since       3.0
  */
-class TracksModelSubroundResults extends FOFModel
+class TracksModelEventResults extends RModelList
 {
 	/**
 	 * Builds the SELECT query
@@ -35,25 +30,25 @@ class TracksModelSubroundResults extends FOFModel
 		$db = $this->getDbo();
 
 		// Current
-		$subround_id = $this->getState('subround_id');
+		$event_id = $this->getState('event_id');
 
 		$query = $db->getQuery(true);
 
-		$query->select('rr.id, rr.individual_id, rr.team_id, rr.subround_id, rr.rank');
+		$query->select('rr.id, rr.individual_id, rr.team_id, rr.event_id, rr.rank');
 		$query->select('rr.performance, rr.bonus_points, rr.comment');
 		$query->select('CASE WHEN CHAR_LENGTH(rr.number) THEN rr.number ELSE pi.number END AS number');
 		$query->select('pi.id AS piid');
 		$query->select('i.first_name, i.last_name');
 
 		$query->from('#__tracks_rounds_results AS rr');
-		$query->join('inner', '#__tracks_projects_subrounds AS sr ON sr.id = rr.subround_id');
+		$query->join('inner', '#__tracks_projects_events AS sr ON sr.id = rr.event_id');
 		$query->join('inner', '#__tracks_projects_rounds AS pr ON pr.id = sr.projectround_id');
 		$query->join('inner', '#__tracks_individuals AS i ON i.id = rr.individual_id');
 		$query->join('inner', '#__tracks_projects_individuals AS pi ON (pi.individual_id = rr.individual_id AND pr.project_id = pi.project_id )');
 		$query->join('left', '#__tracks_teams AS t ON t.id = pi.team_id');
 		$query->join('left', '#__users AS u ON u.id = rr.checked_out');
 
-		$query->where('rr.subround_id = ' . (int) $subround_id);
+		$query->where('rr.event_id = ' . (int) $event_id);
 
 		if (!$overrideLimits)
 		{
@@ -99,27 +94,27 @@ class TracksModelSubroundResults extends FOFModel
 		$filter = $this->getState('filter_state', '');
 		if ($filter != '')
 		{
-			$query->where('rr.subround_id = ' . (int) $subround_id);
+			$query->where('rr.event_id = ' . (int) $event_id);
 		}
 
 		return $query;
 	}
 
-	public function getSubroundInfo()
+	public function getEventInfo()
 	{
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true);
 
-		$query->select('sr.projectround_id, sr.id AS subround_id');
+		$query->select('sr.projectround_id, sr.id AS event_id');
 		$query->select('r.name AS roundname');
 		$query->select('p.name AS projectname');
-		$query->select('srt.name AS subroundname');
+		$query->select('srt.name AS eventname');
 		$query->from('#__tracks_projects_rounds AS pr');
 		$query->join('INNER', '#__tracks_rounds AS r ON r.id = pr.round_id');
-		$query->join('INNER', '#__tracks_projects_subrounds AS sr ON sr.projectround_id = pr.id');
-		$query->join('INNER', '#__tracks_subroundtypes AS srt ON sr.type = srt.id');
+		$query->join('INNER', '#__tracks_projects_events AS sr ON sr.projectround_id = pr.id');
+		$query->join('INNER', '#__tracks_eventtypes AS srt ON sr.type = srt.id');
 		$query->join('INNER', '#__tracks_projects AS p ON p.id = pr.project_id');
-		$query->where('sr.id = ' . $this->getState('subround_id'));
+		$query->where('sr.id = ' . $this->getState('event_id'));
 
 		$db->setQuery($query);
 		$res = $db->loadObject();
@@ -136,9 +131,9 @@ class TracksModelSubroundResults extends FOFModel
 	 *
 	 * @since     1.5
 	 */
-	public function saveranks($cid = array(), $rank, $bonus_points, $performance, $individual, $team, $subround_id)
+	public function saveranks($cid = array(), $rank, $bonus_points, $performance, $individual, $team, $event_id)
 	{
-		$row = $this->getTable('subroundresult');
+		$row = $this->getTable('eventresult');
 
 		// update ordering values
 		for ($i = 0; $i < count($cid); $i++)
@@ -149,7 +144,7 @@ class TracksModelSubroundResults extends FOFModel
 				$row->reset();
 				$row->individual_id = $individual[$i];
 				$row->team_id = $team[$i];
-				$row->subround_id = $subround_id;
+				$row->event_id = $event_id;
 				$row->rank = $rank[$i];
 				$row->bonus_points = $bonus_points[$i];
 				$row->performance = $performance[$i];
@@ -190,8 +185,8 @@ class TracksModelSubroundResults extends FOFModel
 			// get project id
 			$this->_db->setQuery(
 				' SELECT pr.project_id FROM #__tracks_projects_rounds AS pr '
-				. ' INNER JOIN #__tracks_projects_subrounds AS sr ON sr.projectround_id = pr.id '
-				. ' WHERE sr.id=' . $this->subround_id
+				. ' INNER JOIN #__tracks_projects_events AS sr ON sr.projectround_id = pr.id '
+				. ' WHERE sr.id=' . $this->event_id
 			);
 			$this->project_id = $this->_db->loadResult();
 		}
@@ -209,8 +204,8 @@ class TracksModelSubroundResults extends FOFModel
 
 		if ($project_id)
 		{
-			$query = ' INSERT IGNORE INTO #__tracks_rounds_results (individual_id, team_id, subround_id) '
-				. ' SELECT pi.individual_id, pi.team_id, ' . $this->subround_id
+			$query = ' INSERT IGNORE INTO #__tracks_rounds_results (individual_id, team_id, event_id) '
+				. ' SELECT pi.individual_id, pi.team_id, ' . $this->event_id
 				. ' FROM #__tracks_projects_individuals AS pi '
 				. ' WHERE pi.project_id = ' . $project_id;
 			$this->_db->setQuery($query);
