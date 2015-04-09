3fc694d9d34e82d33134630e586ed273e6db29e1
julien <jlv@jlv-solutions.com>
2015-04-09 23:07:24 +0200

    Results module

--- modules/site/mod_tracks_results/helper.php
+++ modules/site/mod_tracks_results/helper.php
@@ -1,22 +1,20 @@
 <?php
 /**
-* @version    $Id: helper.php 123 2008-05-30 09:23:23Z julienv $
-* @package    JoomlaTracks
-* @subpackage ResultsModule
-* @copyright  Copyright (C) 2008 Julien Vonthron. All rights reserved.
-* @license    GNU/GPL, see LICENSE.php
-* Joomla Tracks is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @package     JoomlaTracks
+ * @subpackage  Modules.site
+ * @copyright   Copyright (C) 2008-2015 Julien Vonthron. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
 
-// no direct access
 defined('_JEXEC') or die('Restricted access');
 
-//require_once (JPATH_SITE. '/' .'components'. '/' .'com_tracks'. '/' .'helpers'. '/' .'route.php');
-
+/**
+ * Class modTracksResults
+ *
+ * @package     JoomlaTracks
+ * @subpackage  Modules.site
+ * @since       1.0
+ */
 class modTracksResults
 {
 	/**
@@ -24,81 +22,97 @@ class modTracksResults
 	 *
 	 * @var object
 	 */
-  var $_model = null;
+	var $_model = null;
 
-  /**
+	/**
 	 * Round to display
 	 *
 	 * @var object
 	 */
-  var $_round = null;
+	var $_round = null;
 
-  function _getModel()
-  {
-    if ( $this->_model == null )
-    {
-      require_once (JPATH_SITE. '/' .'components'. '/' .'com_tracks'. '/' .'models'. '/' .'roundresult.php');
-      $this->_model = new TracksModelRoundResult();
-    }
-    return $this->_model;
-  }
-
-  function getList(&$params)
+	protected function _getModel()
 	{
-    $model = $this->_getModel();
-    $round = $this->getRound($params);
-		$result = $model->getSubrounds( $round->projectround_id, $params->get('subroundtype_id') );
-		if ($result) return $result[0];
-		else return null;
+		if ($this->_model == null)
+		{
+			require_once JPATH_SITE . '/components/com_tracks/models/roundresult.php';
+			$this->_model = new TracksModelRoundResult;
+		}
+
+		return $this->_model;
 	}
 
-	function getProject(&$params)
+	public function getList(&$params)
 	{
-	  $model = $this->_getModel();
-    return $model->getProject( $params->get('project_id') );
+		$model = $this->_getModel();
+		$round = $this->getRound($params);
+		$result = $model->getSubrounds($round->projectround_id, $params->get('subroundtype_id'));
+
+		if ($result)
+		{
+			return $result[0];
+		}
+		else
+		{
+			return null;
+		}
 	}
 
-	function getRound(&$params)
+	public function getProject(&$params)
 	{
-    if ( $this->_round ) return $this->_round;
+		$model = $this->_getModel();
 
-    $db = JFactory::getDBO();
+		return $model->getProject($params->get('project_id'));
+	}
 
-    $sql = ' SELECT pr.id AS projectround_id, pr.start_date, pr.end_date, pr.round_id, '
-         . ' r.name, '
-         . ' CASE WHEN CHAR_LENGTH( r.alias ) THEN CONCAT_WS( \':\', r.id, r.alias ) ELSE r.id END AS slug '
-         . ' FROM #__tracks_projects_rounds AS pr '
-         . ' INNER JOIN #__tracks_rounds AS r ON r.id = pr.round_id '
-         . ' WHERE project_id = ' . $params->get('project_id')
-         . '   AND pr.published = 1 '
-         . ' ORDER BY start_date ASC, pr.ordering ASC '
-         ;
-    $db->setQuery( $sql );
-    $rounds = $db->loadObjectList();
+	public function getRound(&$params)
+	{
+		if ($this->_round)
+		{
+			return $this->_round;
+		}
 
-    if ( $db->getErrorNum() ) {
-      echo $db->getErrorMsg();
-    }
+		$db = JFactory::getDBO();
 
-    if ( $rounds )
-    {
-      $this->_round = $rounds[0];
+		$sql = ' SELECT pr.id AS projectround_id, pr.start_date, pr.end_date, pr.round_id, '
+			. ' r.name, '
+			. ' CASE WHEN CHAR_LENGTH( r.alias ) THEN CONCAT_WS( \':\', r.id, r.alias ) ELSE r.id END AS slug '
+			. ' FROM #__tracks_projects_rounds AS pr '
+			. ' INNER JOIN #__tracks_rounds AS r ON r.id = pr.round_id '
+			. ' WHERE project_id = ' . $params->get('project_id')
+			. '   AND pr.published = 1 '
+			. ' ORDER BY start_date ASC, pr.ordering ASC ';
+		$db->setQuery($sql);
+		$rounds = $db->loadObjectList();
 
-      // advance round until round start date is in the future.
-      foreach ($rounds as $r)
-      {
-        if ( $r->start_date != '0000-00-00 00:00:00' )
-        {
-          if ( strtotime($r->start_date) > time() ) {
-            break;
-          }
-        }
-        else {
-          break; // rounds need to have at least a start date for this module !
-        }
-        $this->_round = $r;
-      }
-    }
-    return $this->_round;
-  }
+		if ($db->getErrorNum())
+		{
+			echo $db->getErrorMsg();
+		}
+
+		if ($rounds)
+		{
+			$this->_round = $rounds[0];
+
+			// Advance round until round start date is in the future.
+			foreach ($rounds as $r)
+			{
+				if ($r->start_date != '0000-00-00 00:00:00')
+				{
+					if (strtotime($r->start_date) > time())
+					{
+						break;
+					}
+				}
+				else
+				{
+					break; // rounds need to have at least a start date for this module !
+				}
+
+				$this->_round = $r;
+			}
+		}
+
+		return $this->_round;
+	}
 }
