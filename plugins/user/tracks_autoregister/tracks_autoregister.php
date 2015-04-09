875accf95a4b4de7f33576538869ee1a53ac592b
julien <jlv@jlv-solutions.com>
2015-04-10 01:22:00 +0200

    Auto register plugin

--- plugins/user/tracks_autoregister/tracks_autoregister.php
+++ plugins/user/tracks_autoregister/tracks_autoregister.php
@@ -1,29 +1,43 @@
 <?php
 /**
-* @version    $Id$
-* @package    Tracks
-* @subpackage Autoregister
-* @copyright  Copyright (C) 2010 Julien Vonthron. All rights reserved.
-* @license    GNU/GPL, see LICENSE.php
-* Tracks is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.txt for copyright notices and details.
-*/
+ * @package     JoomlaTracks
+ * @subpackage  Plugins.site
+ * @copyright   Copyright (C) 2008-2015 Julien Vonthron. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
 
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 // Import library dependencies
 jimport('joomla.plugin.plugin');
 
-class plgUserTracks_autoregister extends JPlugin {
+$tracksLoader = JPATH_LIBRARIES . '/tracks/bootstrap.php';
 
-	function plgUserTracks_autoregister(&$subject, $config = array())
+if (!file_exists($tracksLoader))
+{
+	throw new Exception(JText::_('COM_TRACKS_LIB_INIT_FAILED'), 404);
+}
+
+include_once $tracksLoader;
+
+// Bootstraps Tracks
+TrackslibBootstrap::bootstrap();
+
+class plgUserTracks_autoregister extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An optional associative array of configuration settings.
+	 *                             Recognized key values include 'name', 'group', 'params', 'language'
+	 *                             (this list is not meant to be comprehensive).
+	 */
+	public function __construct(&$subject, $config = array())
 	{
 		parent::__construct($subject, $config);
-		// load language file for frontend
+
+		// Load language file for frontend
 		$this->loadLanguage();
 	}
 
@@ -31,34 +45,40 @@ class plgUserTracks_autoregister extends JPlugin {
 	 * Creates a tracks individual for the user
 	 * Method is called after user data is stored in the database
 	 *
-	 * @param array		holds the new user data
-	 * @param boolean		true if a new user is stored
-	 * @param	boolean		true if user was succesfully stored in the database
-	 * @param	string		message
+	 * @param   array    $user     holds the new user data
+	 * @param   boolean  $isnew    true if a new user is stored
+	 * @param   boolean  $success  true if user was succesfully stored in the database
+	 * @param   string   $msg      message
+	 *
+	 * @return void
 	 */
-	function onUserAfterSave($user, $isnew, $success, $msg)
+	public function onUserAfterSave($user, $isnew, $success, $msg)
 	{
 		$app = JFactory::getApplication();
 
 		// Require tracks individual table
-		require_once (JPATH_ADMINISTRATOR. '/' .'components'. '/' .'com_tracks'. '/' .'tables'. '/' .'individual.php');
+		require_once JPATH_ADMINISTRATOR . '/components/com_tracks/tables/individual.php';
+
 		if ($isnew)
 		{
-			$table = Jtable::getInstance('Individual', 'Table');
+			$table = RTable::getAdminInstance('Individual', array(), 'com_tracks');
 
 			if ($this->params->get('map_name', 0) == 0)
 			{
 				$split = strpos($user['name'], ' ');
+
 				if ($split && $this->params->get('split_name', 1))
 				{
 					$table->first_name = substr($user['name'], 0, $split);
 					$table->last_name  = substr($user['name'], $split + 1);
 				}
-				else {
+				else
+				{
 					$table->last_name = $user['name'];
 				}
 			}
-			else {
+			else
+			{
 				$table->last_name = $user['username'];
 			}
 
@@ -77,13 +97,14 @@ class plgUserTracks_autoregister extends JPlugin {
 			}
 
 			$table->user_id   = $user['id'];
-			if ($table->check() && $table->store()) {
 
+			if ($table->check() && $table->store())
+			{
 			}
-			else {
+			else
+			{
 				Jerror::raiseWarning(0, JText::_('Error_while_creating_tracks_individual'));
 			}
 		}
 	}
 }
-?>
