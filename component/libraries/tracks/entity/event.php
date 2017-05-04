1f7896f954dc534dd99098df1fa779b09c6253d7
julien <julien.vonthron@gmail.com>
2017-05-04 13:41:39 +0200

    event entity

---
+++ component/libraries/tracks/entity/event.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Tracks.library
+ * @subpackage  Entity
+ * @copyright   Tracks (C) 2008-2015 Julien Vonthron. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Event Entity.
+ *
+ * @since  __deploy_version__
+ */
+class TrackslibEntityEvent extends TrackslibEntityBase
+{
+	/**
+	 * Return project type
+	 *
+	 * @return TrackslibEntityProject
+	 */
+	public function getProjectRound()
+	{
+		$item = $this->loadItem();
+
+		if (!$item)
+		{
+			return false;
+		}
+
+		return TrackslibEntityProjectround::load($item->projectround_id);
+	}
+}
