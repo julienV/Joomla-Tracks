4f60b0063f54ddca8006991a3e54c7497fd9765a
Julien Vonthron <julien.vonthron@gmail.com>
2020-04-19 12:32:05 +0200

    about page update

--- component/admin/views/about/tmpl/default.php
+++ component/admin/views/about/tmpl/default.php
@@ -11,35 +11,44 @@
 * See COPYRIGHT.php for copyright notices and details.
 */
 
-defined('_JEXEC') or die('Restricted access'); ?>
+defined('_JEXEC') or die('Restricted access');
 
-<div>
-
-<table id="abouttracks">
-	<tr>
-		<td>
+RHelperAsset::load('tracksbackend.css');
+?>
+<div class="about-tracks">
+	<div class="about-tracks__logo">
 		  <img src="<?php echo JURI::root() ?>media/com_tracks/images/tracks_logo_250.png"/>
-		</td>
-		<td>
-		  <p><?php echo JText::_('COM_TRACKS_LICENSE'); ?></p>
-			<p>
-			Please visit our <a href="http://tracks.jlv-solutions.com/forum" target="_blank">forum for support</a>.
-			</p>
-      <p>
-      Tracks logo by <a href="mailto:info@notethis.nl">Sebastiaan Rozendaal</a>, www.notethis.nl.
-      </p>
-			<p>
-			<?php echo JText::_('COM_TRACKS_THANKS'); ?>
-			</p>
-			<h2><?php echo JText::_('COM_TRACKS_Helping'); ?></h2>
-			<p>You can help by <a href="http://extensions.joomla.org/index.php?option=com_mtree&task=viewlink&link_id=4738&Itemid=2">voting and reviewing the component on Joomla extension directory</a>, by making donations, or by contributing with code, graphic, translations, etc...</p>
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+	</div>
+	<div class="about-tracks__content">
+		<h2>License</h2>
+		<p><?php echo JText::_('COM_TRACKS_LICENSE'); ?></p>
+		<p>
+			Please visit our <a href="https://tracks.jlv-solutions.com/forum" target="_blank">forum for support</a>,
+			or <a href="https://www.jlv-solutions.com">our website</a>.
+		</p>
+		<p>I have been a professional freelance developer for Joomla since 2008, feel free to <a href="https://www.jlv-solutions.com">contact me</a> if you need Joomla custom developements.</p>
+
+		<p>
+			Tracks logo by <a href="mailto:info@notethis.nl">Sebastiaan Rozendaal</a>, www.notethis.nl.
+		</p>
+		<p>
+			Thanks!!
+		</p>
+
+		<h2><?php echo JText::_('COM_TRACKS_Helping'); ?></h2>
+
+		<h3>Write a review</h3>
+		<p>You can help by <a href="http://extensions.joomla.org/index.php?option=com_mtree&task=viewlink&link_id=4738&Itemid=2">voting and reviewing the component on Joomla extension directory</a>, by making donations, or by contributing with code, graphic, translations, etc...</p>
+
+		<h3>Donate</h3>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="2314656">
 			<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG_global.gif" border="0" name="submit" alt="">
 			<img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-			</form>
-		</td>
-	</tr>
-</table>
+		</form>
+
+		<h3>Help translating to your language!</h3>
+		<p>You can help by translating Tracks to you language, by using <a href="https://www.transifex.com/JoomlaTranslators/tracks/">transifex.com</a></p>
+	</div>
 </div>
