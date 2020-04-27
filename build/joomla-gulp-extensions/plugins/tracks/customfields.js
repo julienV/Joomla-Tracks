a395dac32f7390502719acc8a2a08c30d4c9588b
Julien Vonthron <julien.vonthron@gmail.com>
2020-04-27 13:08:44 +0200

    Cleaned up build

--- build/joomla-gulp-extensions/plugins/tracks/customfields.js
+++
@@ -1,7 +0,0 @@
-var base = require('../baseplugin');
-var path = require('path');
-
-var name = path.basename(__filename).replace('.js', '');
-var group = path.basename(path.dirname(__filename));
-
-base.addPlugin(group, name);
